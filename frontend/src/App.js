00a8d930f5f8776b1f01026a87f861b38c2139da
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-06-02 12:17:04 +0200

    Updated webiste text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function App() {
           </Container>
         </main>
         <footer className="mt-auto">
-          <div className="text-center"> Michael Kraus | Technische Universität München</div>
+          <div className="text-center"> Michael Kraus | Technical University of Munich | School of Engineering and Design | Ergonomics - Human Factors Engineering</div>
         </footer>
       </div>
     </BrowserRouter>
--- frontend/src/sites/HomePage.js
+++ frontend/src/sites/HomePage.js
@@ -3,9 +3,21 @@ import ChatbBox from "../components/ChatBox";
 
 export default function HomePage() {
     return <div>
-        <h1>Home Page</h1>
+        <h1>VacationBOT</h1>
 
-        <p> Your Website Content</p>
+        <p>Hello,
+            thank you for being part of my master thesis project. By supporting this study, you help AI developers to create better and more user-focused AI applications in the future.
+            You will interact with an artificial intelligence (AI) called VacationBOT. This experimental AI will help you to book a simulated vacation based on your preferences.
+            
+            Please wait for the study instructor before moving on. Please also don`t use or open the application after the study.
+
+            Thank you very much.
+
+            Sincerly
+            Michael Kraus
+
+
+        </p>
         <ChatbBox />
     </div>
 }
