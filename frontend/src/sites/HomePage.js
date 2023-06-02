8881534b19274b7323dab69cd24add7507c6902d
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-06-02 12:27:38 +0200

    homepage changes

--- frontend/src/sites/HomePage.js
+++ frontend/src/sites/HomePage.js
@@ -5,15 +5,15 @@ export default function HomePage() {
     return <div>
         <h1>VacationBOT</h1>
 
-        <p>Hello,
-            thank you for being part of my master thesis project. By supporting this study, you help AI developers to create better and more user-focused AI applications in the future.
-            You will interact with an artificial intelligence (AI) called VacationBOT. This experimental AI will help you to book a simulated vacation based on your preferences.
-            
-            Please wait for the study instructor before moving on. Please also don`t use or open the application after the study.
-
-            Thank you very much.
-
-            Sincerly
+        <p>Hello, <br/>
+            thank you for being part of my master thesis project. By supporting this study, you help AI developers to create better and more user-focused AI applications in the future. <br/>
+            You will interact with an artificial intelligence (AI) called VacationBOT. This experimental AI will help you to book a simulated vacation based on your preferences. <br/>
+            <br/>
+            Please wait for the study instructor before moving on. Please also don`t use or open the application after the study.<br/>
+            <br/>
+            Thank you very much.<br/>
+            <br/>
+            Sincerly<br/>
             Michael Kraus
 
 
