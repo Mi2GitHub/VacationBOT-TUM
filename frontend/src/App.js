a6e213cd66e039598e8b43d1cbcd3a813918b876
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-06-02 12:27:07 +0200

    Name and para changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <header>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-              <Navbar.Brand>VacationBOT</Navbar.Brand>
+              <Navbar.Brand>VacationBOT | Master Thesis Project</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="me-auto w-100 justify-content-end">
