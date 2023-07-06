93ec94110a550d33e5149d9b5665c55ad05bd6e0
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-07-06 22:46:24 +0200

    Update HomePage.js

--- frontend/src/sites/HomePage.js
+++ frontend/src/sites/HomePage.js
@@ -4,7 +4,7 @@ import myImage from "./minilogo.svg"
 
 export default function HomePage() {
     return <div>
-        <h1><img src={myImage}></img>VacationBOT</h1>
+        <img src={myImage}></img>
         <p>Hallo, <br/>
             danke, dass du mich bei dem Projekt meiner Masterarbeit unterstützt. Hierbei hilfst du dabei, dass KI-Entwickler bessere und nutzerzentriertere KI-Anwendungen entwickeln können. <br/>
             In diesem Versuch wirst du mit einer künstlichen Intelligenz (KI) namens VacationBOT interagieren. Diese experimentelle KI-Anwendung wird dich dabei unterstützen, einen simulierten Urlaub, basierend auf deinen Anforderungen und Wünschen, zu finden und zu buchen. <br/>
