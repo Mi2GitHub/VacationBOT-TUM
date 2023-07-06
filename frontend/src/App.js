b1ea950af9398c5efbb6497f5079d325f218698d
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-07-06 20:31:23 +0200

    pc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,7 +36,7 @@ function App() {
           </Container>
         </main>
         <footer className="mt-auto">
-          <div className="text-center"> Michael Kraus | Technical University of Munich | School of Engineering and Design | Ergonomics - Human Factors Engineering</div>
+          <div className="text-center"> Michael KRAUS | Technical University of Munich | School of Engineering and Design | Ergonomics - Human Factors Engineering</div>
         </footer>
       </div>
     </BrowserRouter>
