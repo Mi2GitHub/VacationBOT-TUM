82d04454d92ca3d70a3b801ec97f6467d3e58ed0
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-07-06 22:24:31 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
         <header>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-              <Navbar.Brand><img src={myImage}></img></Navbar.Brand>
+              <Navbar.Brand><img src={myImage}></img>VacationBOT</Navbar.Brand>
               <Navbar.Toggle aria-controls="basic-navbar-nav">
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="me-auto w-100 justify-content-end">
