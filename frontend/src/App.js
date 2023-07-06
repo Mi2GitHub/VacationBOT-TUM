1f5ff2af14ec5f3c512aff21855b39378ef59dc9
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-07-06 21:52:38 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Nav from "react-bootstrap/Nav";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./sites/HomePage";
 import AdminPage from "./sites/AdminPage";
-import myImage from "./VB_long_TUM.svg"
+import myImage from "./VB_Long_TUM.svg"
 
 function App() {
   return (
