73114fc499d22fc32787f172b021f77d6fb4fc34
Mi2GitHub <135318202+Mi2GitHub@users.noreply.github.com>
2023-07-06 22:56:38 +0200

    Update HomePage.js

--- frontend/src/sites/HomePage.js
+++ frontend/src/sites/HomePage.js
@@ -4,12 +4,12 @@ import ChatbBox from "../components/ChatBox";
 export default function HomePage() {
     return <div>
         <p>Hallo, <br/>
-            danke, dass du mich bei dem Projekt meiner Masterarbeit unterstützt. Hierbei hilfst du dabei, dass KI-Entwickler bessere und nutzerzentriertere KI-Anwendungen entwickeln können. <br/>
+            danke, dass du mich bei dem Projekt meiner Masterarbeit unterstützt. Hierdurch hilfst du dabei, dass KI-Entwickler bessere und nutzerzentriertere KI-Anwendungen entwickeln können. <br/>
             In diesem Versuch wirst du mit einer künstlichen Intelligenz (KI) namens VacationBOT interagieren. Diese experimentelle KI-Anwendung wird dich dabei unterstützen, einen simulierten Urlaub, basierend auf deinen Anforderungen und Wünschen, zu finden und zu buchen. <br/>
             <br/>
             Bitte warte auf den Studienleiter, bevor du weitermachst.<br/>
             <br/>
-            Vielen Dank!.<br/>
+            Vielen Dank!<br/>
             <br/>
             Michael Kraus
 
